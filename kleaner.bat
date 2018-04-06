9f858b58dddbd8d613edc7cbe77df3f91ca7e513
DamienKram <38134667+DamienKram@users.noreply.github.com>
2018-04-06 18:42:38 +0300

    Update kleaner.bat

--- kleaner.bat
+++ kleaner.bat
@@ -1,18 +1,20 @@
+REM System dirs
 del /F /S /Q %TEMP%
-
-REM sysdir
-del /F /S /Q "%SystemRoot%\SoftwareDistribution"
+del /F /S /Q "%SystemRoot%\debug"
 del /F /S /Q "%SystemRoot%\Logs"
 del /F /S /Q "%SystemRoot%\Temp"
-del /F /S /Q "%SystemRoot%\debug"
 del /F /S /Q "%SystemRoot%\Prefetch"
 del /F /S /Q "%SystemRoot%\System32\LogFiles"
-del /F /S /Q "%SystemRoot%\Temp"
-REM dllhell
+del /F /S /Q "%SystemRoot%\SoftwareDistribution"
+
+REM !!!Корзина!!!!
+del /F /S /Q "C:\$RECYCLE.BIN"
+
+REM Dll Hell
 del /F /S /Q "%SystemRoot%\WinSxS\Temp\PendingDeletes"
 del /F /S /Q "%SystemRoot%\WinSxS\ManifestCache"
 
-rem winappdata
+rem Windows appdata
 del /F /S /Q "%USERPROFILE%\AppData\Local\CrashDumps"
 del /F /S /Q "%USERPROFILE%\AppData\Local\Package Cache"
 del /F /S /Q "%USERPROFILE%\AppData\Local\Temp"
@@ -22,7 +24,7 @@ del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Explorer"
 del /F /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Recent"
 del /F /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Recent\CustomDestinations"
 
-rem appdata
+rem Apps data Local
 del /F /S /Q "%USERPROFILE%\AppData\Local\Opera\Opera\cache"
 del /F /S /Q "%USERPROFILE%\AppData\Local\Opera\Opera\application_cache"
 del /F /S /Q "%USERPROFILE%\AppData\Local\Opera\Opera\icons"
@@ -33,18 +35,15 @@ del /F /S /Q "%USERPROFILE%\AppData\Local\CrashDumps"
 del /F /S /Q "%USERPROFILE%\AppData\Local\NVIDIA\GLCache"
 del /F /S /Q "%USERPROFILE%\AppData\Local\NVIDIA Corporation"
 del /F /S /Q "%USERPROFILE%\AppData\Local\NuGet\Cache"
-rem appdata roaming
+
+rem Apps data roaming
 del /F /S /Q "%USERPROFILE%\AppData\Roaming\Disk-O\logs"
 del /F /S /Q "%USERPROFILE%\AppData\Roaming\Mozilla\Firefox\Crash Reports"
-rem appdata etc
+
+rem Apps data etc
 del /F /S /Q "%ProgramFiles(x86)%\Steam\logs"
 del /F /S /Q "C:\ProgramData\GOG.com\Galaxy\logs"
 del /F /S /Q "C:\NVIDIA"
 
 
-
-
 pause
-dir "%SystemRoot%\servicing"
-dir "%SystemRoot%\Installer"
-dir "C:\$RECYCLE.BIN"
